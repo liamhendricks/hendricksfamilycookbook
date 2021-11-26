599a03883d4a8fa5098f1ee56554b68f26d1c2c2
Liam Hendricks <ljh.softwaredev@gmail.com>
2021-11-26 13:24:09 -0700

    Updated Chocolate Chip Cookies 400x200 and 52 other files

--- about.markdown
+++
@@ -1,7 +0,0 @@
----
-title: About
-permalink: "/about/"
-layout: page
----
-
-A project for digitizing the entire Hendricks family recipes!
---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Chocolate Chip Cookies",
+        "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
+        "tags": [],
+        "url": "http://hendricksfamilycookbook.siteleaf.net/recipes/chocolate-chip-cookies/"
+      },{
+        "title": "Peanut Butter Cookies",
+        "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
+        "tags": [],
+        "url": "http://hendricksfamilycookbook.siteleaf.net/recipes/peanut-butter-cookies/"
+      },{
+        "title": "Vegan Bolognese",
+        "excerpt":"My plant-based version of a classic, hearty tomato sauce. Instead of the traditional ground beef or pork, we will use lentils and walnuts as a replacement. I personally think it’s one of the few vegan “meat” dishes that tastes better without the meat. Carly and I eat it all the...","categories": ["vegan","pasta"],
+        "tags": ["vegan","pasta"],
+        "url": "http://hendricksfamilycookbook.siteleaf.net/recipes/vegan-bolognese/"
+      },]
