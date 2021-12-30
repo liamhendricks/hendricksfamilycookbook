70aa94f7ff87757c2850f7bb8c766140d416c121
Liam Hendricks <ljh.softwaredev@gmail.com>
2021-12-30 16:01:45 -0700

    Updated Feed and 7 other files

--- assets/javascripts/lunr/lunr.store.js
+++ assets/javascripts/lunr/lunr.store.js
@@ -1,23 +1,8 @@
 var store = [{
-        "title": "Chocolate Chip Cookies",
-        "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
-        "tags": [],
-        "url": "https://hendricksfamilycookbook.com/recipes/chocolate-chip-cookies/"
-      },{
-        "title": "Peanut Butter Cookies",
-        "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
-        "tags": [],
-        "url": "https://hendricksfamilycookbook.com/recipes/peanut-butter-cookies/"
-      },{
-        "title": "Vegan Bolognese",
+        "title": "Liam's Vegan Bolognese",
         "excerpt":"My plant-based version of a classic, hearty tomato sauce. Instead of the traditional ground beef or pork, we will use lentils and walnuts as a replacement. I personally think it’s one of the few vegan “meat” dishes that tastes better without the meat. Carly and I eat it all the...","categories": ["vegan","pasta"],
         "tags": ["vegan","pasta"],
         "url": "https://hendricksfamilycookbook.com/recipes/vegan-bolognese/"
-      },{
-        "title": "Test Recipe",
-        "excerpt":"Tea balls! Ingredients   1 Onion  1 bag of flour  1 gallon of gatoradeDirections   Mix everything together.  Enjoy!","categories": ["drinks"],
-        "tags": ["drinks"],
-        "url": "https://hendricksfamilycookbook.com/recipes/test-recipe/"
       },{
         "title": "Janice's Broccoli Salad",
         "excerpt":"A family favorite from my favorite Aunt Janice! Ingredients Salad   2 bunches broccoli  chopped 1/3 cup chopped red Burmuda onion  1 1/2 lb, bacon (cooked crisp, and crumbled)  3/4 cup grated cheddar cheeseDressing   1 cup mayonnaise (no substitutes)  1/2 cup sugar  3 TBS red wine vinegar","categories": ["Salad"],
