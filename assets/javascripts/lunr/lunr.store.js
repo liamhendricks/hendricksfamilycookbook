0b9485af686fb60b95b8e540c57446def75005b8
Liam Hendricks <ljh.softwaredev@gmail.com>
2021-12-30 16:36:51 -0700

    Updated Pxl 20211220 044656524 and 7 other files

--- assets/javascripts/lunr/lunr.store.js
+++ assets/javascripts/lunr/lunr.store.js
@@ -8,4 +8,19 @@ var store = [{
         "excerpt":"A family favorite from my favorite Aunt Janice! Ingredients Salad   2 bunches broccoli  chopped 1/3 cup chopped red Burmuda onion  1 1/2 lb, bacon (cooked crisp, and crumbled)  3/4 cup grated cheddar cheeseDressing   1 cup mayonnaise (no substitutes)  1/2 cup sugar  3 TBS red wine vinegar","categories": ["Salad"],
         "tags": ["broccoli"],
         "url": "https://hendricksfamilycookbook.com/recipes/janice-broccoli-salad/"
+      },{
+        "title": "Grandma Eliason's Oreo Dessert",
+        "excerpt":"A must-have for Deer Valley reunions. Recipe from Grandma Eliason. Ingredients 1 package oreo cookies 1 lg. Cool Whip 2-4 TBS melted butter 1 gallon chocolate chip ice cream, softenedChocolate Syrup 16-oz. pkg. chocolate chips 1 cube butter 1 1/3 cups evaporated milk 2 cups powdered sugar 1 tsp. vanillaDirections...","categories": ["dessert"],
+        "tags": ["desseret","oreo"],
+        "url": "https://hendricksfamilycookbook.com/recipes/oreo-dessert/"
+      },{
+        "title": "Grandma Eliason's Tea Balls",
+        "excerpt":"Liam’s favorite cookie! They are simple, only take a few ingredients, and are quick to bake in the oven. Ingredients 1 cup unsalted butter (softened) 1 tsp vanilla 1/2 cup powdered sugar (plus more for garnish) 2 1/4 cups flour 1/4 tsp salt (optional) 3/4 cup crushed walnuts, pecans or...","categories": ["cookies","dessert"],
+        "tags": ["cookies","dessert"],
+        "url": "https://hendricksfamilycookbook.com/recipes/gmas-tea-balls/"
+      },{
+        "title": "Pattie Shumway's Won Ton Salad",
+        "excerpt":"Flavorful salad by Pattie Shumway. ##Ingredients 1 head of lettuce 2 or 3 cooked chicken breasts, cubed 1/2 pkg. won ton skins, fried and crumbled 1/2 bunch green onions, chopped 2-3 TB chopped parsley 2 TB sunflower seedsDressing 1/4 cup sugar 1/4 cup oil 1/4 cup vinegar 1/2 tsp. accent...","categories": ["salad"],
+        "tags": ["salad","won ton","chicken"],
+        "url": "https://hendricksfamilycookbook.com/recipes/pattie-shumway-won-ton-salad/"
       },]
