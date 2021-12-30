d72b4194115550ffcfc9b7936152aa9917df353f
Liam Hendricks <ljh.softwaredev@gmail.com>
2021-12-30 16:00:04 -0700

    Updated Janice Broccoli Salad and 7 other files

--- assets/javascripts/lunr/lunr.store.js
+++ assets/javascripts/lunr/lunr.store.js
@@ -20,7 +20,7 @@ var store = [{
         "url": "https://hendricksfamilycookbook.com/recipes/test-recipe/"
       },{
         "title": "Janice's Broccoli Salad",
-        "excerpt":"A family favorite from my favorite Aunt Janice! Ingredients   2 bunches broccoli  chopped 1/3 cup chopped red Burmuda onion  1 1/2 lb, bacon (cooked crisp, and crumbled)  3/4 cup grated cheddar cheeseDressing   1 cup mayonnaise (no substitutes)  1/2 cup sugar  3 TBS red wine vinegar","categories": ["Salad"],
+        "excerpt":"A family favorite from my favorite Aunt Janice! Ingredients Salad   2 bunches broccoli  chopped 1/3 cup chopped red Burmuda onion  1 1/2 lb, bacon (cooked crisp, and crumbled)  3/4 cup grated cheddar cheeseDressing   1 cup mayonnaise (no substitutes)  1/2 cup sugar  3 TBS red wine vinegar","categories": ["Salad"],
         "tags": ["broccoli"],
-        "url": "https://hendricksfamilycookbook.com/recipes/Janice's%20Broccoli%20Salad/"
+        "url": "https://hendricksfamilycookbook.com/recipes/janice-broccoli-salad/"
       },]
