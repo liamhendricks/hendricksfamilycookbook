bb252a58909536c777a96f67fac24882eab2bc6b
catherinecandles@gmail.com <catherinecandles@gmail.com>
2021-12-30 17:28:04 -0700

    Updated Ginger Snap Cookies and 7 other files

--- assets/javascripts/lunr/lunr.store.js
+++ assets/javascripts/lunr/lunr.store.js
@@ -10,7 +10,7 @@ var store = [{
         "url": "https://hendricksfamilycookbook.com/recipes/janice-broccoli-salad/"
       },{
         "title": "Grandma Eliason's Oreo Dessert",
-        "excerpt":"A must-have for Deer Valley reunions. Recipe from Grandma Eliason. Ingredients 1 package oreo cookies 1 lg. Cool Whip 2-4 TBS melted butter 1 gallon chocolate chip ice cream, softenedChocolate Syrup 16-oz. pkg. chocolate chips 1 cube butter 1 1/3 cups evaporated milk 2 cups powdered sugar 1 tsp. vanillaDirections...","categories": ["dessert"],
+        "excerpt":"A must-have for Deer Valley reunions. Recipe from Grandma Eliason. Zoe’s fav. Ingredients 1 package oreo cookies 1 lg. Cool Whip 2-4 TBS melted butter 1 gallon chocolate chip ice cream, softenedChocolate Syrup 16-oz. pkg. chocolate chips 1 cube butter 1 1/3 cups evaporated milk 2 cups powdered sugar 1...","categories": ["dessert"],
         "tags": ["desseret","oreo"],
         "url": "https://hendricksfamilycookbook.com/recipes/oreo-dessert/"
       },{
@@ -20,7 +20,17 @@ var store = [{
         "url": "https://hendricksfamilycookbook.com/recipes/gmas-tea-balls/"
       },{
         "title": "Pattie Shumway's Won Ton Salad",
-        "excerpt":"Flavorful salad by Pattie Shumway. ##Ingredients 1 head of lettuce 2 or 3 cooked chicken breasts, cubed 1/2 pkg. won ton skins, fried and crumbled 1/2 bunch green onions, chopped 2-3 TB chopped parsley 2 TB sunflower seedsDressing 1/4 cup sugar 1/4 cup oil 1/4 cup vinegar 1/2 tsp. accent...","categories": ["salad"],
+        "excerpt":"Flavorful and light salad by Pattie Shumway. Can be used as main or side dish. Ingredients 1 head of lettuce 2 or 3 cooked chicken breasts, cubed 1/2 pkg. won ton skins, fried and crumbled 1/2 bunch green onions, chopped 2-3 TB chopped parsley 2 TB sunflower seedsDressing 1/4 cup...","categories": ["salad"],
         "tags": ["salad","won ton","chicken"],
         "url": "https://hendricksfamilycookbook.com/recipes/pattie-shumway-won-ton-salad/"
+      },{
+        "title": "Guittard Chocolate Chip Cookies",
+        "excerpt":"Catherine’s favorite chocolate chip cookie recipe. Ingredients 1 cup butter 3/4 cup sugar 3/4 cup brown sugar 2 large eggs 1 tsp vanilla 1 tsp salt 1 tsp soda 2 1/2 - 3 cups flour 2 cups Guittard chocolate chips (milk or semi-sweet)Directions Preheat oven to 375 degrees. Cream wet...","categories": ["cookies"],
+        "tags": ["cookies","chocholate chip"],
+        "url": "https://hendricksfamilycookbook.com/recipes/guittard-chocolate-chip-cookies/"
+      },{
+        "title": "Ginger Snap Cookies",
+        "excerpt":"Recipe from Jenny Sampson, a friend from the neighborhood. Ingredients 1 1/2 cups butter 2 cups sugar 1/2 cup molasses 4 tsp soda 4 cups flour 2 tsp cinnamon 1/2 tsp salt 2-2 1/2 tsp gingerDirections Preheat oven to 350 degrees. Mix wet, then dry ingredients. Chill dough. Bake for...","categories": ["cookies"],
+        "tags": ["cookies","ginger snap"],
+        "url": "https://hendricksfamilycookbook.com/recipes/ginger-snap-cookies/"
       },]
